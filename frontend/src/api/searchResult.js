d2108837e4c5851d0d33dd99b5308b5f16c7a88f
Manish Kumar <manishkumar9576700@gmail.com>
2026-08-26 13:20:24 +0530

    feat: enhance video search with loading state and fixed opening and closing of search results container.

--- frontend/src/api/searchResult.js
+++ frontend/src/api/searchResult.js
@@ -1,8 +1,15 @@
 import { getApi } from "@/services/axios";
 
-export const fetchSearchResults = async (query) => {
-  const api = getApi();
-  const { data } = await api.get(`/search?q=${query}`);
-  console.log(data)
-  return data.results;
+export const fetchSearchResults = async (query, setIsLoading) => {
+  try {
+    setIsLoading(true);
+    const api = getApi();
+    const { data } = await api.get(`/search?q=${query}`);
+    return data.results;
+    // console.log(data);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    setIsLoading(false);
+  }
 };
--- frontend/src/components/videoPlayer/RoomControls.jsx
+++ frontend/src/components/videoPlayer/RoomControls.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Video } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -9,17 +9,35 @@ export default function RoomControls() {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const containerRef = useRef(null);
 
   const handleVideoSubmit = async (e) => {
     e.preventDefault();
     if (!query.trim()) return;
-    const results = await fetchSearchResults(query);
+    const results = await fetchSearchResults(query, setIsLoading);
     setResults(results);
     setQuery("");
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [containerRef]);
+
   return (
-    <div className="load-video-container relative">
+    <div ref={containerRef} className="load-video-container relative">
       <form onSubmit={handleVideoSubmit} className="url-input-group">
         <Input
           className="search-input"
@@ -38,6 +56,11 @@ export default function RoomControls() {
           style={{ padding: "5px" }}
           className="search-result-container flex flex-col items-center gap-4 overflow-y-auto absolute bg-zinc-900 h-100 w-full top-[120%] backdrop-blur-2xl z-99 rounded"
         >
+          {isLoading && (
+            <div className="flex items-center justify-center h-full w-full">
+              <p>Loading...</p>
+            </div>
+          )}
           {results?.map((video) => (
             <VideoCard key={video.id} video={video} setIsOpen={setIsOpen} />
           ))}
